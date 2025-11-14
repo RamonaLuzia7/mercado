786d7b9061487a9de665f9babf983f37e0a8ceb7
RamonaLuzia7 <ramonaluzia17@gmail.com>
2025-11-14 14:23:56 -0300

    alteracao nos controles

--- src/main/java/br/com/senai/backend/sistema_mercado/controlles/FuncionarioController.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/controlles/FuncionarioController.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.senai.backend.sistema_mercado.models.Funcionario;
 import br.com.senai.backend.sistema_mercado.services.FuncionarioService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("funcionario")
